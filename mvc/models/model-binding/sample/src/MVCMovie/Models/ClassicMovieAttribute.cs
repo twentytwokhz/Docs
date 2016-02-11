a01d445d754515f6b498a250592ac04ea43c04c1
RachelAppel <rachelap@microsoft.com>
2016-02-10 19:30:23 -0500

    modifications to sample, added custom attributes

---
+++ mvc/models/model-binding/sample/src/MVCMovie/Models/ClassicMovieAttribute.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNet.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace MVCMovie.Models
+{
+    public class ClassicMovieAttribute : ValidationAttribute, IClientModelValidator
+    {
+        private int _year;
+        public ClassicMovieAttribute(int Year)
+        {
+            _year = Year;                
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            Movie movie = (Movie)validationContext.ObjectInstance;
+
+            if (movie.Genre == Genre.Classic)
+            {
+                if (movie.ReleaseDate.Year < this._year)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult("Classic movies must have a release year earlier than " + this._year);
+                }
+            }
+            return ValidationResult.Success;
+        }
+
+        IEnumerable<ModelClientValidationRule> IClientModelValidator.GetClientValidationRules(ClientModelValidationContext context)
+        {
+            var rule = new ModelClientValidationRule("classicmovie","Classic movies must have a release year earlier than " + this._year);
+            yield return rule;
+        }
+    }
+
+
+
+}
+
+    
+
---
+++ mvc/models/model-binding/sample/src/MVCMovie/Models/FamilyMovieAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.Mvc.ModelBinding;
+using Microsoft.AspNet.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCMovie.Models
+{
+    public class FamilyMovieAttribute : ValidationAttribute, IClientModelValidator
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            Movie movie = (Movie)validationContext.ObjectInstance;
+
+            if (movie.Audience == "G")
+            {
+                if (movie.Genre != Genre.Family)
+                {
+                    return new ValidationResult("Movies for the G [General] audience must be Family movies");
+                }
+            }
+            return ValidationResult.Success;
+        }
+        IEnumerable<ModelClientValidationRule> IClientModelValidator.GetClientValidationRules(ClientModelValidationContext context)
+        {          
+            var rule = new ModelClientValidationRule("familymovie", "Movies for the G [General] audience must be Family movies");
+            rule.ValidationParameters.Add("genre", "7");
+            rule.ValidationParameters.Add("audience", "G");
+            yield return rule;            
+        }
+    }
+}
